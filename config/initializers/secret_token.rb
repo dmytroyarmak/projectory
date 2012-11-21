3af5fc3598492463a9e6ba24f8de7ba3dc01bae7
Dima <0979029562@mail.ru>
2012-11-21 19:50:44 +0200

    create new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectory::Application.config.secret_token = 'c9f5c9cb83f50074733292d51f9d9f2de22f09a4e3cc19f191d71181b9901bb582b7658a8cea551b56f14fd448c5d215ea828b4f9fe2b1a21b8627f0360bb740'
