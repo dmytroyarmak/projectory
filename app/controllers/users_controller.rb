fefa0afca4259a64db795becc2c2a635aeaeb893
Dima <0979029562@mail.ru>
2012-11-25 18:01:41 +0200

    add guest user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = params[:user] ? User.new(params[:user]) : User.new_guest
     if @user.save
+      current_user.move_to(@user) if current_user && current_user.guest?
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Thank you for signing up!"
     else
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   attr_accessible :name
   has_many :tasks, :dependent => :destroy, :order => "priority DESC"
+  belongs_to :user
   validates :name, :presence => true
 end
